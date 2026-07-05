0522812fa4654e2791033b846b97d197c6befde0
liminjun <v-leeli@hotmail.com>
2026-07-05 20:05:53 +0800

    feat:Java条件语句学习

---
+++ 02-java-for-programming-beginners/module3/IfElse.java
@@ -0,0 +1,10 @@
+public class IfElse {
+    public static void main(String[] args) {
+        int number = 3;
+        if (number > 5) {
+            System.out.println("The number is greater than 5");
+        } else {
+            System.out.println("The number is not greater than 5.");
+        }
+    }
+}
---
+++ 02-java-for-programming-beginners/module3/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int number = 10;
+        if (number > 5) {
+            System.out.println("The number is greater than 5");
+        }
+    }
+}
